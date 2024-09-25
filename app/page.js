a8a444fda9004a8dbb3571cfbc6e9f94db0e369e
Kamalshakoor <kamalshakoor29022000@gmail.com>
2024-09-25 14:37:01 +0500

    added headersection with slider and main section on homepage

--- app/page.js
+++ app/page.js
@@ -1,16 +1,55 @@
-import Link from 'next/link';
+import Link from "next/link";
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/sliderShow/image-slideshow";
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      
-      <p>
-        <Link href="/meals/">MealsPage</Link>
-        <Link href="/meals/share/">ShareMeal</Link>
-        <Link href="/community/">community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Next Level Food for <br /> Next Level People </h1>
+
+            <p>Delicious foods, shared by a food-loving community.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the community</Link>
+            <Link href="/meals">Browse meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            Foodies is a platform for food enthusiasts to share their favorite
+            recipes and connect with like-minded food enthusiasts. You can browse 
+            recipes, create your own recipes, and share them with others.
+          </p>
+          <p>
+            Nextlevel Food is a platform for food enthusiasts to share their favorite
+          </p>
+        </section>
+        <section className={classes.section}>
+          <h2>Why choose Nextlevel Food?</h2>
+          <p>
+            Nextlevel Food is a platform for food enthusiasts to share their favorite
+            recipes and connect with like-minded food enthusiasts. You can browse 
+            recipes, create your own recipes, and share them with others.
+          </p>
+        </section>
+        <section className={classes.section}>
+          <h2>Features</h2>
+          <ul>
+            <li>Share your favorite recipes</li>
+            <li>Connect with like-minded food enthusiasts</li>
+            <li>Discover new recipes and share them with others</li>
+          </ul>
+        </section>
+
+      </main>
+    </>
   );
 }
--- components/header/main-header.js
+++ components/header/main-header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import LogoImg from '../assets/logo.png'
+import LogoImg from '../../assets/logo.png'
 import classes from './main-header.module.css'
 
 const MainHeader = () => {
