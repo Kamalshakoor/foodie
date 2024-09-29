0d3274cf88eec2715a2d849cea9c0edadd24205b
Kamalshakoor <kamalshakoor29022000@gmail.com>
2024-09-29 19:26:16 +0500

    saved share meal form data in database

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,9 @@
+import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
 
 export default function ShareMealPage() {
-  return (
+   return (
     <>
       <header className={classes.header}>
         <h1>
@@ -11,7 +12,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
